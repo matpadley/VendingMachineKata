34f60f9bc5a42ce2508b645f2c0fe2719249a812
Mat <matthew.padley@christie.nhs.uk>
2020-01-20 15:04:30 +0000

    Coin rejection and display

--- VendingMachine.Tests/UnitTest1.cs
+++ VendingMachine.Tests/UnitTest1.cs
@@ -1,18 +1,89 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using FluentValidation;
+using Moq;
 using NUnit.Framework;
+using VendingMachine.Coin;
 
 namespace VendingMachine.Tests
 {
-    public class Tests
+    public class Vending_Machine_Init_Tests: VendingMachineBaseTest
     {
+        private IVendingMachine _vendingMachine;
+        private ICoinService _coinService;
+        
         [SetUp]
         public void Setup()
         {
+            _coinService = new CoinService();
+            
+            _vendingMachine = new VendMachine(_coinService);
         }
 
         [Test]
-        public void Test1()
+        public void Machine_Should_Display_Correct_Message_On_Start()
         {
-            Assert.Pass();
+            Assert.AreEqual("INSERT COIN", _vendingMachine.GetDisplay());
         }
+
+        [Test]
+        public void Penny_Should_Be_Rejected_Placed_In_Coin_Return()
+        {
+            _vendingMachine.InsertCoin(Penny);
+            
+            Assert.AreEqual(1, _vendingMachine.GetReturnTray().Count);
+            Assert.AreEqual(0, _vendingMachine.GetCurrentAmount());
+            Assert.AreEqual("INSERT COIN", _vendingMachine.GetDisplay());
+        }
+    }
+
+    public class VendMachine : IVendingMachine
+    {
+        private readonly ICoinService _coinService;
+        
+        private readonly IValidator _coinValidator;
+        
+        private ICollection<CoinAttributes> _coinReturn;
+        
+        private ICollection<CoinAttributes> _insertedCoins;
+
+        public VendMachine(ICoinService coinService)
+        {
+            _coinValidator = new CoinValidator();
+            _coinReturn = new Collection<CoinAttributes>();
+            _insertedCoins = new Collection<CoinAttributes>();
+            _coinService = coinService;
+        }
+        
+        public string GetDisplay()
+        {
+            return "INSERT COIN";
+        }
+
+        public void InsertCoin(CoinAttributes coinAttributes)
+        {
+            _coinService.GetCoin(coinAttributes);
+
+            if (_coinValidator.Validate(coinAttributes.Coin).IsValid)
+            {
+                _insertedCoins.Add(coinAttributes);
+            }
+            else
+            {
+                _coinReturn.Add(coinAttributes);
+            }
+        }
+
+        public ICollection<CoinAttributes> GetReturnTray() => _coinReturn;
+        public double GetCurrentAmount() => _insertedCoins.Sum(coin => coin.MonetaryValue);
+    }
+
+    interface IVendingMachine
+    {
+        string GetDisplay();
+        void InsertCoin(CoinAttributes coinAttributes);
+        ICollection<CoinAttributes> GetReturnTray();
+        double GetCurrentAmount();
     }
 }
---
+++ VendingMachine.Tests/VendingMachineBaseTest.cs
@@ -0,0 +1,12 @@
+﻿using VendingMachine.Coin;
+
+namespace VendingMachine.Tests
+{
+    public abstract class VendingMachineBaseTest
+    {
+        protected CoinAttributes Nickel => new CoinAttributes(2, 21.12m, 1.95m);
+        protected CoinAttributes Dime => new CoinAttributes(2.268m, 17.91m, 1.35m);
+        protected CoinAttributes Penny => new CoinAttributes(2.5m, 19.05m, 1.52m);
+        protected CoinAttributes Quarter => new CoinAttributes(6.25m, 24.26m, 1.75m);
+    }
+}
