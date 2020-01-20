50d2be38613af47ec31c5e63ed96b3fea6c165fc
Mat <matthew.padley@christie.nhs.uk>
2020-01-20 13:45:39 +0000

    Coin Validator

---
+++ VendingMachine/Coin.cs
@@ -0,0 +1,12 @@
+using System.Reflection.Metadata.Ecma335;
+
+namespace VendingMachine
+{
+    public enum Coin
+    {
+        Nickel,
+        Penny,
+        Quarter,
+        Dime
+    }
+}
---
+++ VendingMachine/ValidateCoinTests.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using NUnit.Framework;
+
+namespace VendingMachine
+{
+    public class ValidateCoinTests
+    {
+        private IValidator _coinValidator;
+        
+        [SetUp]
+        public void Setup()
+        {
+            _coinValidator = new CoinValidator();
+        }
+
+        [TestCase(Coin.Nickel, true)]
+        [TestCase(Coin.Dime, true)]
+        [TestCase(Coin.Quarter, true)]
+        [TestCase(Coin.Penny, false)]
+        public void Only_Accept_Valid_Coins(Coin coin, bool accept)
+        {
+            Assert.AreEqual(accept, _coinValidator.Validate(coin).IsValid);
+        }
+
+    }
+
+    public class CoinValidator : AbstractValidator<Coin>
+    {
+        // This would go to a data store or some other validation end point, hopefully for multi curreny
+        public CoinValidator()
+        {
+            RuleFor(coin => coin).NotEqual(Coin.Penny);
+        }
+    }
+}
